0ec5358504ab51aff6256b98f51d29e540012ce8
2-propanol <nuclear.fusion.247@gmail.com>
2021-11-10 17:24:43 +0900

    add: decode_range, remove: angleindex_xy_to_image

--- btf_extractor/ubo2014.pyx
+++ btf_extractor/ubo2014.pyx
@@ -5,7 +5,7 @@ from typing import Any, Tuple
 cimport cython
 import numpy as np
 cimport numpy as np
-from cython.parallel import parallel, prange
+from cython.parallel import prange
 
 from btf_extractor.c_ext.btf cimport BTF, Spectrum, Vector3
 from btf_extractor.c_ext.btf cimport BTFFetchSpectrum, DestroyBTF, LoadBTF
@@ -20,11 +20,21 @@ cdef class Ubo2014:
     角度は全て度数法(degree)を用いている。
     画像の実体はopencvと互換性のあるndarray形式(BGR, channels-last)で出力する。
 
+    `col_decode_range`、`row_decode_range`は負の値による指定も可能。
+    `img_shape`の範囲外を指定しようとすると`ValueError`を返す。
+    `*_decode_range`の書き換えで`img_shape`は変更されない。
+
     Attributes:
-        btf_filepath (str): コンストラクタに指定したbtfファイルパス。
-        img_shape (tuple[float,float,float]): btfファイルに含まれている画像のshape。
+        btf_filepath (str):
+            コンストラクタに指定したbtfファイルパス。読み取り専用。
+        img_shape (tuple[float,float,float]):
+            btfファイルに含まれている画像のshape。読み取り専用。
         angles_set (set[tuple[float,float,float,float]]):
-            btfファイルに含まれる画像の角度条件の集合。
+            btfファイルに含まれる画像の角度条件の集合。読み取り専用。
+        col_decode_range (tuple[int,int]):
+            `angles_to_image()`でデコードする列の範囲。読み書き可能。
+        row_decode_range (tuple[int,int]):
+            `angles_to_image()`でデコードする行の範囲。読み書き可能。
 
     Example:
         >>> btf = Ubo2014("carpet01_resampled_W400xH400_L151xV151.btf")
@@ -38,12 +48,28 @@ cdef class Ubo2014:
         (400, 400, 3)
         >>> print(image.dtype)
         float32
+        >>> btf.row_decode_range = (136, 264)
+        >>> btf.col_decode_range = (136, -136)
+        >>> print(btf.angles_to_image(*angles_list[0]).shape)
+        (128, 128, 3)
+        >>> print(btf.img_shape)
+        (400, 400, 3)
+        >>> btf.row_decode_range = (136, 401)
+        ValueError: out of range
+        >>> btf.row_decode_range = ("136", "264")
+        TypeError: indices must be integers
+        >>> btf.row_decode_range = (136, 264, 1)
+        ValueError: too many values to unpack (expected 2)
     """
     cdef readonly str btf_filepath
     cdef BTF* __raw_btf
     cdef readonly tuple img_shape
     cdef readonly frozenset angles_set
     cdef readonly dict angles_vs_index_dict
+    cdef Py_ssize_t row_decode_begin
+    cdef Py_ssize_t row_decode_length
+    cdef Py_ssize_t col_decode_begin
+    cdef Py_ssize_t col_decode_length
 
     def __cinit__(self, btf_filepath: str) -> None:
         """使用するbtfファイルを指定する"""
@@ -58,6 +84,10 @@ cdef class Ubo2014:
 
         # self.__raw_btf.ChannelCountを3と仮定する。
         self.img_shape = (self.__raw_btf.Width, self.__raw_btf.Height, 3)
+        self.row_decode_begin = 0
+        self.row_decode_length = self.__raw_btf.Width
+        self.col_decode_begin = 0
+        self.col_decode_length = self.__raw_btf.Height
         cdef Py_ssize_t num_views = self.__raw_btf.ViewCount
         cdef Py_ssize_t num_lights = self.__raw_btf.LightCount
 
@@ -83,22 +113,16 @@ cdef class Ubo2014:
             _views_in_cartesian[view_idx,2] = view.z
 
         # *_in_spherical: 球面座標系で格納された角度情報
-        cdef np.ndarray[DTYPE_F32_t, ndim=2] lights_in_spherical = np.empty(
-            (num_lights, 3), dtype=DTYPE_F32
-        )
-        lights_in_spherical = self._cartesian_to_spherical(
-            _lights_in_cartesian
+        cdef np.ndarray[DTYPE_F32_t, ndim=2] lights_in_spherical = (
+            self._cartesian_to_spherical(_lights_in_cartesian)
         )
 
-        cdef np.ndarray[DTYPE_F32_t, ndim=2] views_in_spherical = np.empty(
-            (num_views, 3), dtype=DTYPE_F32
-        )
-        views_in_spherical = self._cartesian_to_spherical(
-            _views_in_cartesian
+        cdef np.ndarray[DTYPE_F32_t, ndim=2] views_in_spherical = (
+            self._cartesian_to_spherical(_views_in_cartesian)
         )
 
-        cdef list angles_list
-        angles_list = [
+        # cdef list angles_list
+        cdef list angles_list = [
             (float(l[0]), float(l[1]), float(v[0]), float(v[1]))
             for l, v in product(lights_in_spherical, views_in_spherical)
         ]
@@ -123,12 +147,57 @@ cdef class Ubo2014:
         self, np.ndarray[DTYPE_F32_t, ndim=2] xyz
     ):
         """直交座標系から球面座標系に変換する"""
-        # UBO2014の角度情報の精度は0.5度単位なので小数点以下1桁に丸める
         cdef np.ndarray[DTYPE_F32_t, ndim=1] theta
         cdef np.ndarray[DTYPE_F32_t, ndim=1] phi
         theta = np.rad2deg(np.arccos(xyz[:,2]))
-        phi = np.rad2deg(np.arctan2(xyz[:,1], xyz[:,0])) % 360  # -180から+180を、0から360に収める
+        # -180から+180を、0から360に収める
+        phi = np.rad2deg(np.arctan2(xyz[:,1], xyz[:,0])) % 360
+        # UBO2014の角度情報の精度は0.5度単位なので小数点以下1桁に丸める
         return np.array((theta, phi)).T.round(1)
+
+    @property
+    def row_decode_range(self) -> Tuple[int, int]:
+        return (self.row_decode_begin, self.row_decode_end)
+
+    @row_decode_range.setter
+    def row_decode_range(self, row_index: Tuple[int, int]) -> None:
+        if not (type(row_index[0]) is int and type(row_index[1]) is int):
+            raise TypeError("indices must be integers")
+
+        cdef int left, right
+        left, right = row_index
+        if left < 0:
+            left = self.__raw_btf.Width + left
+        if right < 0:
+            right = self.__raw_btf.Width + right
+
+        if right < self.__raw_btf.Width and left < right:
+            self.row_decode_begin = left
+            self.row_decode_length = right - left
+        else:
+            raise ValueError("out of range")
+
+    @property
+    def col_decode_range(self) -> Tuple[int, int]:
+        return (self.col_decode_begin, self.col_decode_end)
+
+    @col_decode_range.setter
+    def col_decode_range(self, col_index: Tuple[int, int]) -> None:
+        if not (type(col_index[0]) is int and type(col_index[1]) is int):
+            raise TypeError("indices must be integers")
+
+        cdef int top, bottom
+        top, bottom = col_index
+        if top < 0:
+            top = self.__raw_btf.Height + top
+        if bottom < 0:
+            bottom = self.__raw_btf.Height + bottom
+
+        if bottom < self.__raw_btf.Height and top < bottom:
+            self.col_decode_begin = top
+            self.col_decode_length = bottom - top
+        else:
+            raise ValueError("out of range")
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
@@ -136,25 +205,28 @@ cdef class Ubo2014:
     cdef np.ndarray[DTYPE_F32_t, ndim=3] _index_to_image_skip_validation(
         self, int light_idx, int view_idx
     ):
-        cdef Py_ssize_t height = self.__raw_btf.Height
-        cdef Py_ssize_t width = self.__raw_btf.Width
-        cdef int lidx = light_idx
-        cdef int vidx = view_idx
+        cdef Py_ssize_t col_begin = self.col_decode_begin
+        cdef Py_ssize_t row_begin = self.row_decode_begin
+        cdef Py_ssize_t height = self.col_decode_length
+        cdef Py_ssize_t width = self.row_decode_length
+        cdef Py_ssize_t lidx = light_idx
+        cdef Py_ssize_t vidx = view_idx
         cdef Spectrum spec
         cdef np.ndarray[DTYPE_F32_t, ndim=3] img3d = np.empty(
             (height, width, 3), dtype=DTYPE_F32
         )
         cdef float[:, :, ::1] img3d_view = img3d
         cdef int x, y
-        with nogil, parallel():
-            for y in prange(height, schedule="dynamic"):
-                for x in range(width):
-                    spec = BTFFetchSpectrum(self.__raw_btf, lidx, vidx, x, y)
-                    img3d_view[y,x,0] = spec.z
-                    img3d_view[y,x,1] = spec.y
-                    img3d_view[y,x,2] = spec.x
+        for y in prange(height, nogil=True, schedule="dynamic"):
+            for x in range(width):
+                spec = BTFFetchSpectrum(
+                    self.__raw_btf, lidx, vidx, x+row_begin, y+col_begin
+                )
+                img3d_view[y,x,0] = spec.z
+                img3d_view[y,x,1] = spec.y
+                img3d_view[y,x,2] = spec.x
         return img3d
-    
+
     def angles_to_image(
         self, tl: float, pl: float, tv: float, pv: float
     ) -> np.ndarray:
@@ -173,34 +245,6 @@ cdef class Ubo2014:
     ) -> np.ndarray:
         """`light_idx`, `view_idx`で指定される角度条件の画像をndarray形式で返す
 
-        `light_idx`, `view_idx`で指定される角度条件の、(`x`, `y`)座標の画素値をRGBのfloatで返す
-
         値(引数)の妥当性をチェックしない。不正な値を入力するとクラッシュする。
         """
         return self._index_to_image_skip_validation(light_idx, view_idx)
-
-    @cython.boundscheck(False)
-    @cython.wraparound(False)
-    @cython.nonecheck(False)
-    cdef np.ndarray[DTYPE_F32_t, ndim=1] _index_xy_to_image_skip_validation(
-        self, int light_idx, int view_idx, int x, int y
-    ):
-        cdef Spectrum spec
-        cdef np.ndarray[DTYPE_F32_t, ndim=1] rgb = np.empty(
-            (3), dtype=DTYPE_F32
-        )
-        spec = BTFFetchSpectrum(self.__raw_btf, light_idx, view_idx, x, y)
-        rgb[0] = spec.x
-        rgb[1] = spec.y
-        rgb[2] = spec.z
-        return rgb
-
-    @cython.nonecheck(False)
-    def _angleindex_xy_to_image_unsafe(
-        self, light_idx: int, view_idx: int, x: int, y: int
-    ) -> np.ndarray:
-        """画素単位アクセス
-
-        値(引数)の妥当性をチェックしない。不正な値を入力するとクラッシュする。
-        """
-        return self._index_xy_to_image_skip_validation(light_idx, view_idx, x, y)
